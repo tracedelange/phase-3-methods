6f8e3f6f1f1da38b1491b6579fb7ebbdd79300e7
Trace <tracedelange@me.com>
2021-08-10 09:16:05 -0500

    commit tree 55c0034729a5ae5a308f8b1aa29607d0f7bb25d2

--- methods.rb
+++ methods.rb
@@ -1,1 +1,37 @@
 # Your code here!
+
+#js -> rb
+# function myFunction(param) {
+#   console.log("Running myFunction");
+#   return param + 1;
+# }
+
+def my_method(param)
+    puts "Running my method"
+    param + 1
+end
+
+def greet_programmer
+    puts "Hello, programmer!"
+end
+
+def greet(name)
+    puts "Hello, #{name}!"
+end
+
+def greet_with_default(name = "programmer")
+    puts "Hello, #{name}!"
+end
+
+def add(num1, num2)
+    num1 + num2
+end
+
+def halve(number)
+    puts number.class
+    if number.class != Integer
+        return nil
+    end
+
+    return number /2
+end
